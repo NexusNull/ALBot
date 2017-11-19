e3162371471a9788dd4efcb1f1d24922bf51d89c
NexusNull <p.wellershaus@googlemail.com>
2017-11-19 12:55:38 +0100

    Added respawn code for when the character starts dead, Removed auto drop off code.

--- CODE/default.js
+++ CODE/default.js
@@ -2,18 +2,20 @@ var reviving = false;
 var targetMonster = "crab";
 var fighting = false;
 
-function find(name, level) {
-    for (var i in character.items) {
-        if (character.items[i] != null && character.items[i].name == name)
-            return i;
-    }
-    return -1;
+if(character.rip){
+    setTimeout(function(){
+        respawn();
+        setTimeout(function(){
+            smart_move(targetMonster, function () {
+                fighting = true;
+            });
+        },1000);
+    },12000);
+} else {
+    smart_move(targetMonster, function () {
+        fighting = true;
+    });
 }
-
-
-smart_move(targetMonster, function () {
-    fighting = true;
-});
 
 setInterval(function () {
     if (!fighting) {
@@ -22,6 +24,7 @@ setInterval(function () {
 
     if (character.rip && !reviving) {
         reviving = true;
+        fighting = false;
         var position = {
             x: character.real_x,
             y: character.real_y,
@@ -31,41 +34,11 @@ setInterval(function () {
             respawn();
             setTimeout(function () {
                 reviving = false;
-                smart_move(position);
+                smart_move(position, function(){
+                    fighting = true;
+                });
             }, 1000)
         }, 12000)
-    }
-
-
-
-
-    if ( character.esize < 5 || (find("hpot0") === -1 || character.items[find("hpot0")].q < 100) || (find("mpot0") === -1 || character.items[find("mpot0")].q < 100) ) {
-        fighting = false;
-        smart_move({x: -130, y: -100}, function () {
-            buy("hpot0", 9998-((find("hpot0")=== -1)?0:character.items[find("hpot0")].q));
-            buy("mpot0", 9998-((find("mpot0")=== -1)?0:character.items[find("mpot0")].q));
-
-            let i = 0;
-            let sendTo = "Emerald";
-            let itemBlacklist = ["hpot0", "hpot1", "mpot0", "mpot1", "luckbooster", "goldbooster", "xpbooster"]
-            let reciever = get_player(sendTo);
-            let interval = setInterval(function () {
-                if(i >= 42){
-                    clearInterval(interval);
-                    smart_move(targetMonster,function(){
-                        fighting = true;
-                    });
-                }
-                if (reciever && distance(character, reciever) < 200) {
-                    let item = character.items[i];
-                    if (item && itemBlacklist.indexOf(item.name) === -1) {
-                        send_item(sendTo, i, (item.q) ? item.q : 1);
-                    }
-                }
-                i++;
-            }, 250);
-        });
-        return;
     }
 
     if (can_use("hp") && (character.hp / character.max_hp <= .50) || (character.max_hp - character.hp > 200)) {
@@ -85,7 +58,9 @@ setInterval(function () {
         if (can_attack(target)) {
             attack(target);
         } else {
-            move(target.real_x, target.real_y);
+            let dist = Math.sqrt(Math.pow(target.real_x-character.real_x,2)+Math.pow(target.real_y-character.real_y,2))
+            if(dist > character.range-20)
+                move((target.real_x+character.real_x)/2, (target.real_y+character.real_y)/2);
         }
     }
 }, 1000 / 4);
