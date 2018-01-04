8d5927d89dea54ce823bf5a2650be0d6c944c37f
NexusNull <p.wellershaus@googlemail.com>
2018-01-04 15:36:13 +0100

    Fixing memory leak.

--- Executor.js
+++ Executor.js
@@ -4,52 +4,57 @@
 var fs = require("fs");
 var vm = require("vm");
 
-var common_functions =fs.readFileSync('modedGameFiles/common_functions.js')+'\n';
-var runner_functions =fs.readFileSync('modedGameFiles/runner_functions.js')+'\n';
+var common_functions = fs.readFileSync('modedGameFiles/common_functions.js') + '\n';
+var runner_functions = fs.readFileSync('modedGameFiles/runner_functions.js') + '\n';
 
 
-
-var Executor = function(glob,file){
+var Executor = function (glob, file) {
 
     var intervals = [];
     var timeouts = [];
 
     var sandbox = {
-        global:global,
+        global: global,
         character: glob.character,
-        G:glob.G,
-        active:false,
-        catch_errors:true,
-        is_code:1,
-        is_server:0,
-        is_game:0,
-        parent:glob,
-        console:console,
-        setInterval:function () {
-            //console.log("Register Interval")
+        G: glob.G,
+        active: false,
+        catch_errors: true,
+        is_code: 1,
+        is_server: 0,
+        is_game: 0,
+        parent: glob,
+        console: console,
+        setInterval: function () {
             intervals.push(setInterval.apply(null, arguments));
         },
-        setTimeout:function () {
-            //console.log("Register Timeout")
-            timeouts.push(setTimeout.apply(null, arguments));
-        }
+        setTimeout: function () {
+            var i;
+            var timeoutArguments = arguments;
+            let timeout = setTimeout(function () {
+                timeoutArguments[0].apply(null, Array.from(timeoutArguments).slice(2));
+                delete timeouts[i];
+            }, timeoutArguments[1]);
+            i = timeouts.push(timeout) - 1;
+            return timeout;
+        },
+
     };
     vm.createContext(sandbox);
     glob.sandbox = sandbox;
 
-    var script = new vm.Script(common_functions+runner_functions+fs.readFileSync('CODE/'+file)+'');
+    var script = new vm.Script(common_functions + runner_functions + fs.readFileSync('CODE/' + file) + '');
 
-    this.execute = function(){
-        console.log("Executing "+file);
+    this.execute = function () {
+        console.log("Executing " + file);
         glob.code_active = true;
         try {
             script.runInNewContext(sandbox);
-        } catch(error){
+        } catch (error) {
             console.log(error)
         }
     };
 
-    this.stop = function(){
+    this.stop = function () {
         glob.code_active = false;
         intervals.forEach(clearInterval);
         timeouts.forEach(clearTimeout);
