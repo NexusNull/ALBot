8ccb73b0e9860e632643341a452e77388dc2ec82
NexusNull <p.wellershaus@googlemail.com>
2021-04-25 14:46:34 +0200

    fetch game data

--- GameDataManager.js
+++ GameDataManager.js
@@ -1,4 +1,24 @@
 const fs = require("fs");
+const path = require("path")
+const gameFiles = [
+    "/js/pixi/fake/pixi.min.js",
+    "/js/libraries/combined.js",
+    "/js/codemirror/fake/codemirror.js",
+    "/js/common_functions.js",
+    "/js/functions.js",
+    "/js/game.js",
+    "/js/html.js",
+    "/js/payments.js",
+    "/js/keyboard.js",
+    "/data.js"
+];
+
+const runnerFiles = [
+    "/js/common_functions.js",
+    "/js/runner_functions.js",
+    "/js/runner_compat.js"
+];
+
 
 class GameDataManager {
     constructor(httpWrapper) {
@@ -18,9 +38,21 @@ class GameDataManager {
     }
 
     async updateGameData() {
-        let data = await this.httpWrapper.getGameData();
         let gameVersion = await this.httpWrapper.getGameVersion();
-        fs.writeFileSync("./data/data." + gameVersion + ".json", JSON.stringify(data));
+
+        for (let gameFile of gameFiles) {
+            let data = await this.httpWrapper.getFile(gameFile);
+            let localPath = path.join("data", gameVersion, gameFile)
+            fs.mkdirSync(path.parse(localPath).dir, {recursive: true})
+            fs.writeFileSync(localPath, data)
+        }
+
+        for (let runnerFile of runnerFiles) {
+            let data = await this.httpWrapper.getFile(runnerFile);
+            let localPath = path.join("data", gameVersion, runnerFile)
+            fs.mkdirSync(path.parse(localPath).dir, {recursive: true})
+            fs.writeFileSync(localPath, data)
+        }
     }
 
 }
