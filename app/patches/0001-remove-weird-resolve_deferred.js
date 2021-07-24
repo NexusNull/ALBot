ce8154d1a84689efa088749026ea2cbb3ec13867
NexusNull <p.wellershaus@googlemail.com>
2021-07-24 21:08:59 +0200

    implement "weird resolve" patch

--- app/patches/0001-remove-weird-resolve_deferred.js
+++ app/patches/0001-remove-weird-resolve_deferred.js
@@ -6,7 +6,12 @@ const fs = require("fs");
  * @param files
  */
 function run(files) {
-    console.log("Dummy patch")
+    let asd = files["/js/common_functions.js"].split("console.error(\"Weird resolve_deferred issue: \"+name),console.log(\"If you emit socket events manually, ignore this message\");")
+    if (asd.length === 2) {
+        files["/js/common_functions.js"] = asd.join(";")
+    } else {
+        console.error("Unable to find correct needle, aborting ...")
+    }
 }
 
 
