9792f12697ae2c6f86546087408fddaffc778cb9
NexusNull <p.wellershaus@googlemail.com>
2021-09-08 23:16:38 +0200

    fix null check patch

--- app/patches/0004-add-null-check-localstorage-cm.js
+++ app/patches/0004-add-null-check-localstorage-cm.js
@@ -3,11 +3,26 @@
  * @param files
  */
 function run(files) {
-    let asd = files["/js/runner_functions.js"].split("data[2]=new Date(data[2]);\n\t\t\t\tmessages.push(data);")
-    if (asd.length === 2) {
-        files["/js/runner_functions.js"] = asd.join("if(data){data[2]=new Date(data[2]);\n\t\t\t\tmessages.push(data);}")
-    } else {
-        console.error("Unable to find correct needle, aborting ...")
+    const safeCopy = files["/js/runner_functions.js"];
+    let success = true;
+
+    let tmp = files["/js/runner_functions.js"];
+
+    tmp = tmp.split("data[2]=new Date(data[2]);\n\t\t\t\tmessages.push(data);")
+    if (tmp.length === 2) {
+        tmp = tmp.join("if(data){\n\t\t\t\t\tdata[2]=new Date(data[2]);\n\t\t\t\t\tmessages.push(data);\t\t\t\n}")
+    } else success = false;
+
+    tmp = tmp.split("var keys=Object.keys(localStorage);")
+    if (tmp.length === 2) {
+        tmp = tmp.join("var keys=localStorage._keys")
+    } else success = false;
+
+    if (success) {
+        console.error("Unable to find correct needle, aborting ...");
+        files["/js/runner_functions.js"] = safeCopy;
+    } else{
+        files["/js/runner_functions.js"] = tmp;
     }
 }
 
